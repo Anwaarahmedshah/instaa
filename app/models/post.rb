0de3e903f3c7df6a8c3b6da2dfbcbad0a2bf2b7c
Shahzaib Waqars <shahzaibwaqars@gmail.com>
2017-05-04 13:06:51 +0500

    first

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+  validates :image, presence: true
+  validates :user_id, presence: true 
+
+  belongs_to :user  
+
+
+  # has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
+end
